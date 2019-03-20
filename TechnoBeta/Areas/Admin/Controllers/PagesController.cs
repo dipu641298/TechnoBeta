bde51fce0f90a49cb30b0c35302d3aa29cd24792
dipu98 <rahman641298@gmail.com>
2019-03-20 20:25:29 +0600

    Admin AddPage done

---
+++ TechnoBeta/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechnoBeta.Models.Data;
+using TechnoBeta.Models.ViewModels.Pages;
+
+namespace TechnoBeta.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // declare list of PageVM
+            List<PageVM> pagesList;
+
+
+            // initialize the list
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            // return view with the list
+            return View(pagesList);
+        }
+
+        [HttpGet]
+        // GET: Admin/Pages/AddPage
+        public ActionResult AddPage()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        // POST: Admin/Pages/AddPage
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check model state
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+
+                // Declare slug
+                string slug;
+
+                // Init PageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO title
+                dto.Title = model.Title;
+
+                // Check for and set slug if needed
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // Make sure title and slug are unique
+                if(db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "Title or Slug already exists");
+                    return View(model);
+                }
+
+                // DTO the reset
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                // Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            // Set the TempData message
+            TempData["SM"] = "You have added a new page.";
+
+            // Redirect
+            return RedirectToAction("AddPage");
+
+        }
+    }
+}
---
+++ TechnoBeta/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using TechnoBeta.Models.Data;
+
+namespace TechnoBeta.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+
+        }
+
+        public PageVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Body = row.Body;
+            Slug = row.Slug;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength (50, MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
