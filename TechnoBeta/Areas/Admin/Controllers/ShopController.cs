8a9b77539cf57379f40ab6133429c3a62f7b96b8
dipu98 <rahman641298@gmail.com>
2019-03-27 22:34:34 +0600

    Commit X+3

--- TechnoBeta/Areas/Admin/Controllers/ShopController.cs
+++ TechnoBeta/Areas/Admin/Controllers/ShopController.cs
@@ -22,5 +22,68 @@ namespace TechnoBeta.Areas.Admin.Controllers
 
             return View(categoryVMList);
         }
+
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            string id;
+
+            using (Db db = new Db())
+            {
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                CategoryDTO dto = new CategoryDTO();
+
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                id = dto.Id.ToString();
+            }
+
+            return id;
+        }
+
+
+       /* [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+
+                CategoryDTO dto;
+
+                for (int i = 0; i < id.Length; i++)
+                {
+                    int catId = id[i];
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+
+        }*/
+
+
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                CategoryDTO dto = db.Categories.Find(id);
+
+                db.Categories.Remove(dto);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Categories");
+        }
     }
 }
