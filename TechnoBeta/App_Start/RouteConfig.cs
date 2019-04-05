3c3de6ea4c3833c63158c27ea1825c20ed894354
dipu98 <rahman641298@gmail.com>
2019-04-06 01:04:52 +0600

    HomePage has started

--- TechnoBeta/App_Start/RouteConfig.cs
+++ TechnoBeta/App_Start/RouteConfig.cs
@@ -13,11 +13,16 @@ namespace TechnoBeta
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "TechnoBeta.Controllers" });
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "TechnoBeta.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "TechnoBeta.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "TechnoBeta.Controllers" });
+
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
         }
     }
 }
