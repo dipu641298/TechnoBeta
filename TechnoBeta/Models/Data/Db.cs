e43e3e4d3cc3d017266737faa27cd46262c4eb0f
dipu98 <rahman641298@gmail.com>
2019-04-08 21:50:53 +0600

    Order started

--- TechnoBeta/Models/Data/Db.cs
+++ TechnoBeta/Models/Data/Db.cs
@@ -15,6 +15,8 @@ namespace TechnoBeta.Models.Data
         public DbSet<UserDTO> Users { get; set; }
         public DbSet<RoleDTO> Roles { get; set; }
         public DbSet<UserRoleDTO> UserRoles { get; set; }
+        public DbSet<OrderDTO> Orders { get; set; }
+        public DbSet<OrderDetailsDTO> OrderDetails { get; set; }
 
     }
 }
